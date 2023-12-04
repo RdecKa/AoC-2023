eaadbf0d9f06aa76b9ef26189c81d95a552938be
Karin Frlic <karin.frlic@gmail.com>
2023-12-04 22:50:59 +0100

    Update template

--- dayNN/solution.py
+++ dayNN/solution.py
@@ -2,6 +2,11 @@ from aoclib.puzzle import Puzzle
 
 
 class DayNN(Puzzle):
+    def __init__(self, filename):
+        super().__init__(filename)
+        self.star1_solution = None
+        self.star2_solution = None
+
     def star1(self):
         return 0
 
