8a7b847562c747f47875429bedce637ffefc6030
Karin Frlic <karin.frlic@gmail.com>
2023-12-05 21:56:52 +0100

    Add solution for day 5, star 2

--- day05/solution.py
+++ day05/solution.py
@@ -1,3 +1,4 @@
+from operator import itemgetter
 import re
 from aoclib.puzzle import Puzzle
 
@@ -7,16 +8,53 @@ rule_regex = re.compile(r"([0-9]+) ([0-9]+) ([0-9]+)")
 
 class AlmanacMap:
     def __init__(self) -> None:
-        self.rules: (int, int, int) = []
+        self.rules: (int, int, int) = []  # (dest_start, source_start, range_len)
 
     def add_rule(self, rule: (int, int, int)):
         self.rules.append(rule)
+
+    def initialise(self):
+        # Sort by source_start
+        self.rules = sorted(self.rules, key=itemgetter(1))
+
+        # Add missing identity rules (x => x)
+        next_to_map = 0
+        new_maps = []
+        for _, source_start, range_len in self.rules:
+            if source_start > next_to_map:
+                new_maps.append((next_to_map, next_to_map, source_start - next_to_map))
+            next_to_map = source_start + range_len
+
+        # The last identity map is not included (because we don't know the length)
+        self.rules = sorted(self.rules + new_maps, key=itemgetter(1))
 
     def map(self, source: int) -> int:
         for dest_start, source_start, range_len in self.rules:
             if source_start <= source < source_start + range_len:
                 return dest_start + (source - source_start)
         return source
+
+    def map_range(self, input_range):
+        (range_start, remaining_len) = input_range
+        mapped_ranges = []
+        for dest_start, source_start, range_len in self.rules:
+            if remaining_len == 0:
+                break
+            if source_start <= range_start < source_start + range_len:
+                mapped_range_len = min(
+                    range_len - (range_start - source_start), remaining_len
+                )
+                mapped_ranges.append(
+                    (dest_start + (range_start - source_start), mapped_range_len)
+                )
+                remaining_len -= mapped_range_len
+                range_start += mapped_range_len
+        if remaining_len > 0:
+            # This is identity mapping after the last rule
+            mapped_ranges.append(
+                ((range_start + remaining_len - remaining_len), remaining_len)
+            )
+        return mapped_ranges
 
 
 class Almanac:
@@ -29,6 +67,21 @@ class Almanac:
         for m in self.maps:
             dest = m.map(dest)
         return dest
+
+    def map_ranges(self):
+        dest_ranges = self.get_seed_ranges()
+        for m in self.maps:
+            new_ranges = []
+            for r in dest_ranges:
+                new_ranges += m.map_range(r)
+            dest_ranges = new_ranges
+        return dest_ranges
+
+    def get_seed_ranges(self):
+        ranges = []
+        for i in range(0, len(self.seeds), 2):
+            ranges.append((self.seeds[i], self.seeds[i + 1]))
+        return ranges
 
 
 def parse_input(lines):
@@ -50,6 +103,7 @@ def parse_input(lines):
             except StopIteration:
                 end_reached = True
                 break
+        almanac_map.initialise()
         maps.append(almanac_map)
     return Almanac(seeds, maps)
 
@@ -60,10 +114,11 @@ class Day5(Puzzle):
         self.almanac = parse_input(self.filereader.lines())
 
         self.star1_solution = 111627841
-        self.star2_solution = None
+        self.star2_solution = 69323688
 
     def star1(self):
         return min(self.almanac.map(seed) for seed in self.almanac.seeds)
 
     def star2(self):
-        return 0
+        lowest_range = min(self.almanac.map_ranges(), key=itemgetter(0))
+        return lowest_range[0]
