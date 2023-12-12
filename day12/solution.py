f79722c5a5c9a8cf4f1b1a2827094b67bac0f1a8
Karin Frlic <karin.frlic@gmail.com>
2023-12-12 22:04:31 +0100

    Add (too) slow solution for day 12, star 2

--- day12/solution.py
+++ day12/solution.py
@@ -11,6 +11,34 @@ def parse_input_line(line: str):
     return spring_row, groups
 
 
+def parse_folded_input_line(line: str):
+    folded_spring_row, folded_groups_str = line.split(" ")
+    spring_row = "?".join([folded_spring_row] * 5)
+    groups_str = ",".join([folded_groups_str] * 5)
+    groups = [int(g) for g in groups_str.split(",")]
+    return spring_row, groups
+
+
+def handle_defect_group(row: str, start_pos: int, groups: list[int]):
+    contiguous = groups[0]
+    end_pos = start_pos + contiguous - 1
+    if end_pos >= len(row):
+        return 0
+    for spring in row[start_pos + 1 : end_pos + 1]:
+        if spring == OPERATIONAL:
+            # All springs should be either defect or unknown
+            return 0
+    if end_pos == len(row) - 1:
+        # Group is ending at the end of the row
+        if len(groups) == 1:
+            return 1
+        return 0
+    # Defected groups must be separated by at least one operating spring
+    if row[end_pos + 1] == DEFECT:
+        return 0
+    return count_possible_configurations(row, end_pos + 2, groups[1:])
+
+
 def count_possible_configurations(row: str, start_pos: int, groups: list[int]):
     if len(groups) == 0:
         for spring in row[start_pos:]:
@@ -20,39 +48,23 @@ def count_possible_configurations(row: str, start_pos: int, groups: list[int]):
     if start_pos >= len(row):
         # len(groups) > 0 ==> not possible
         return 0
+    remaining_space = len(row) - start_pos
+    needed_space = sum(groups) + len(groups) - 1
+    if remaining_space < needed_space:
+        return 0
 
     if row[start_pos] == OPERATIONAL:
-        return count_possible_configurations(row, start_pos + 1, groups)
+        # Jump over all operational spring
+        skip = 1
+        while start_pos + skip < len(row) and row[start_pos + skip] == OPERATIONAL:
+            skip += 1
+        return count_possible_configurations(row, start_pos + skip, groups)
     if row[start_pos] == DEFECT:
-        contiguous = groups[0]
-        end_pos = start_pos + contiguous - 1
-        if end_pos >= len(row):
-            return 0
-        for spring in row[start_pos + 1 : end_pos + 1]:
-            if spring == OPERATIONAL:
-                # All springs should be either defect or unknown
-                return 0
-        if end_pos == len(row) - 1:
-            # Group ending at the end of the row
-            return count_possible_configurations(
-                row, start_pos + contiguous, groups[1:]
-            )
-        # Defected groups must be separated by at least one operating spring
-        if row[end_pos + 1] == OPERATIONAL:
-            return count_possible_configurations(
-                row, start_pos + contiguous, groups[1:]
-            )
-        if row[end_pos + 1] == DEFECT:
-            return 0
-        row = row[: end_pos + 1] + OPERATIONAL + row[end_pos + 1 + 1 :]
-        return count_possible_configurations(row, start_pos + contiguous, groups[1:])
+        return handle_defect_group(row, start_pos, groups)
+
     # row[start_pos] == UNKNOWN
-    row_operational = row[:start_pos] + OPERATIONAL + row[start_pos + 1 :]
-    count_if_operational = count_possible_configurations(
-        row_operational, start_pos, groups
-    )
-    row_defect = row[:start_pos] + DEFECT + row[start_pos + 1 :]
-    count_if_defect = count_possible_configurations(row_defect, start_pos, groups)
+    count_if_operational = count_possible_configurations(row, start_pos + 1, groups)
+    count_if_defect = handle_defect_group(row, start_pos, groups)
     return count_if_operational + count_if_defect
 
 
@@ -70,4 +82,8 @@ class Day12(Puzzle):
         return total
 
     def star2(self):
-        return 0
+        rows = self.filereader.lines(parse_folded_input_line)
+        total = 0
+        for row, groups in rows:
+            total += count_possible_configurations(row, 0, groups)
+        return total
